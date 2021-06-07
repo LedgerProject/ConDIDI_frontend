aefd54d5823ff5874665467994bccbb5e3d650a2
Tim Moehring <tim.moehring@prime-force.com>
2021-06-07 13:24:47 +0200

    remove old project files

--- src/config.js
+++
@@ -1,6 +0,0 @@
-let data = {
-    host: "http://localhost",
-    port: "8080",
-    path: "/api"
-}
-module.exports = { data }
--- src/media/index.js
+++
@@ -1,6 +0,0 @@
-import { IconCalendar } from './icons/icon-calendar';
-import { IconHome } from './icons/icon-home';
-import { IconSettings } from './icons/icon-settings';
-import { IconAvatar } from './icons/icon-avatar';
-
-export { IconCalendar, IconHome, IconSettings, IconAvatar }
