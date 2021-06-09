b65f7e154b179cfdf707a71551dae863819bc97d
Tim Moehring <tim.moehring@prime-force.com>
2021-06-09 15:44:54 +0200

    wip service worker config

--- workbox-config.js
+++ workbox-config.js
@@ -2,7 +2,7 @@ module.exports = {
   globDirectory: "dist/",
   globPatterns: ["**/*.{css,ico,svg,png,jpg,jpeg,webp,html,js,txt,woff,woff2}"],
   ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
-  swDest: "public/condidi/sw.js",
+  swDest: "public/sw.js",
   skipWaiting: true,
   clientsClaim: true,
   runtimeCaching: [
