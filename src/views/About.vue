d422ccfe308d177ad66a903d675f2f1675c4da3d
Tim Moehring <tim.moehring@prime-force.com>
2021-06-09 20:54:42 +0200

    add about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,8 +1,18 @@
 <template>
   <v-container class="fill-height">
-    <v-row justify="center" align="center" class="fill-height">
-      <v-col cols="12" class="text-center">
-        <h1 class="text-h1">About Page</h1>
+    <v-row justify="center" align="center">
+      <v-col cols="12" class="text-start">
+        <h1 class="text-h2">ConDIDI - Conference Digital Identifier Integration</h1>
+      </v-col>
+      <v-col cols="12">
+        <h2 class="text-h4"> Full lifecycle decentralized conference participant management </h2>
+      </v-col>
+      <v-col>
+        <p class="text-h6 font-weight-light">ConDIDI will allow conference organisers to issue digital credentials of attendance to participants, building on the leading role the Technische Informationsbibliothek (TIB) is playing in Germany in introducing unique researcher, publication and conference IDs, and improving on the state of the art of digital credentials. This will allow the participants have digital proof of participation (participant, presenter, chair) and for the organiser to easily manage participation and credential issuing and revocation.</p>
+
+        <p class="text-h6 font-weight-light">The MVP will be a frontend for managing the participant list and combined with a decentralized identity (DID) and credential issuing wallet backend, based on the pioneering work of Jolocom. Credentials issued will follow international standards like OpenBadges and thus be easily included into existing or future digital CV management systems.</p>
+
+        <p class="text-h6 font-weight-light">Implementation of the full credential lifecycle of issuing and retraction as infrastructure will also benefit other areas and is not restricted to this application.</p>
       </v-col>
     </v-row>
   </v-container>
--- src/views/legal/DataProtection.vue
+++ src/views/legal/DataProtection.vue
@@ -1,83 +1,8 @@
 <template>
   <v-container>
-    Privacy Statement
-
-    Data protection is of high priority for the Technische Informationsbibliothek (TIB). As a general rule, the use of TIB’s services does not require the provision of any personal data. However, the processing of personal data may be required where a data subject wants to use special services via the TIB’s web pages. Where the processing of personal data is required and where there is no legal basis for such processing, we shall obtain the data subject’s consent.
-
-    The processing of personal data, such as for example the data subject’s name, address, email address, or telephone number shall always be carried out in accordance with the General Data Protection Regulation (GDPR) and the state and institution-specific data protection rules and regulations applicable to the TIB. This privacy statement serves to inform the public about the nature, scope and purpose of the personal data we collect, use and process, as well as of the rights data subjects are entitled to.
-
-    The terms used in this privacy statement are to be understood within the meaning of the European General Data Protection Regulation (GDPR).
-    Name and address of the controller
-
-    The controller under data protection law shall be:
-
-    Technische Informationsbibliothek (TIB)
-    Welfengarten 1 B
-    30167 Hannover
-    Germany
-
-    Phone: +49 511 762-8989
-    Email: information@tib.eu
-    Website: www.tib.eu
-    Name and address of the data protection officer
-
-    The data protection officer of the controller shall be:
-
-    Elke Brehm
-    Phone: +49 511 762-8138
-    Email: datenschutztibeu
-
-    Postal address:
-
-    Technische Informationsbibliothek (TIB)
-    data protection officer
-    Welfengarten 1 B
-    30167 Hannover
-    Germany
-
-    Visiting Address:
-
-    TIB Conti-Campus
-    Königsworther Platz 1 B
-    30167 Hannover
-
-    Any data subject may contact our data protection officer directly regarding any and all questions and suggestions regarding data protection at any time.
-    Contact possibility via the web pages
-
-    The TIB’s web pages contain information and forms that enable quick electronic contacting via contact form or by email providing name and email address also for customers not registered with the TIB. The indication of name and email address is necessary to establish contact following clarification of the data subject’s concerns. The personal data transmitted by the data subject to the TIB via email or via a contact form are automatically stored internally and only for the purpose of processing or contacting the data subject.
-    Cookies
-
-    This website does not use cookies.
-    Use of web analysis tools
-
-    For the creation of the web statistics we use the open source software Matomo (formerly PIWIK) in anonymized form. This means that no personal data is processed. The use of cookies in the web analysis software Matomo is deactivated.
-
-    When saving the user IP address, the last two octets are not processed. The collection of the user ID is deactivated. For the analysis, the following data is collected in addition to the access to the site and the anonymized IP address: Date and time of the request, page title of the requested page, URL of the previously requested page (referrer URL), screen resolution of the client system, local time zone, URL of clicked and downloaded files, URL of clicked external domains, geolocation of the client (country, region, city), main language of the used browser, user agent of the used browser.
-
-    Matomo
-
-    The information generated with Matomo about the use of this website is processed and stored exclusively with the TIB.
-
-    In this case, an opt-out cookie is placed in the browser of the data subject, which prevents Matomo from storing usage data. When the cookies are deleted, the Matomo opt-out cookie is also deleted. Such objection (opt-out) must be redeclared when visiting the TIB website again
-    Collection of general data and information (logfiles)
-
-    Whenever a data subject calls the TIB web pages, these pages automatically collect information in so-called server log files, which your browser automatically transmits to the TIB. This is:
-
-    Browser type and browser version
-    Operating system used
-    Referrer URL
-    Hostname of the accessing computer
-    IP address
-    Internet service provider of the accessing system
-    Time of the server request
-
-    As a general rule, this data is not attributable to a particular person. This data will not be merged with other data sources.
-    Routine deletion and blocking of personal data
-
-    The TIB processes and stores the data subject’s personal data only for the period necessary to achieve the purpose of such storage and in accordance with the General Data Protection Regulation and the country and institution-specific data protection regulations applicable to the TIB. Thereafter, the personal data will routinely be blocked or deleted in accordance with the statutory provisions.
-    Rights of the data subject
-
-    You shall at any time be entitled to obtain information about the data stored in this library, its origin and recipient and about the purpose of such data processing, as well as to rectification or erasure or restriction of processing or – to the extent that such processing is based on your consent – a right of withdrawal, possibly a right of objection and the right to data portability. Complaints may be lodged with the above-mentioned supervisory authority. You can contact us at any time for further questions on the subject of personal data.
+    <div id="c225" class="frame frame-default frame-type-shortcut frame-layout-0"><div id="c2001" class="frame frame-default frame-type-header frame-layout-0"><header><h1 class="">
+      Privacy Statement
+    </h1></header></div><div id="c240" class="frame frame-default frame-type-textmedia frame-layout-0"><div class="ce-textpic ce-right ce-intext"><div class="ce-bodytext"><p>Data protection is of high priority for the Technische Informationsbibliothek (TIB). As a general rule, the use of TIB’s services does not require the provision of any personal data. However, the processing of personal data may be required where a data subject wants to use special services via the TIB’s web pages. Where the processing of personal data is required and where there is no legal basis for such processing, we shall obtain the data subject’s consent.</p><p>The processing of personal data, such as for example the data subject’s name, address, email address, or telephone number shall always be carried out in accordance with the General Data Protection Regulation (GDPR) and the state and institution-specific data protection rules and regulations applicable to the TIB. This privacy statement serves to inform the public about the nature, scope and purpose of the personal data we collect, use and process, as well as of the rights data subjects are entitled to.</p><p>The terms used in this privacy statement are to be understood within the meaning of the European General Data <a href="https://eur-lex.europa.eu/legal-content/DE/TXT/?uri=CELEX%3A32016R0679" target="_blank" class="external-link-new-window">Protection Regulation (GDPR).</a></p><h3>Name and address of the controller</h3><p>The controller under data protection law shall be:</p><p>Technische Informationsbibliothek (TIB)<br> Welfengarten 1 B<br> 30167 Hannover<br> Germany</p><p>Phone: +49 511 762-8989<br> Email: <a href="javascript:linkTo_UnCryptMailto(%27iwehpk6ejbkniwpekjWpex%3Aaq%25.%2CSaxoepa6%27);">information@tib.eu </a><br> Website: <a href="https://projects.tib.eu/" target="_blank" class="internal-link">www.tib.eu</a></p><h3>Name and address of the data protection officer</h3><p>The data protection officer of the controller shall be:</p><p>Elke Brehm<br> Phone: +49 511 762-8138<br> Email: <a href="javascript:linkTo_UnCryptMailto(%27iwehpk6zwpajoydqpvWpex%3Aaq%27);">datenschutz<span class="tib-mail-a"></span>tib<span class="tib-mail-b"></span>eu</a></p><p><strong>Postal address:</strong></p><p>Technische Informationsbibliothek (TIB)<br> data protection officer<br> Welfengarten 1 B<br> 30167 Hannover<br> Germany</p><p><strong>Visiting Address:</strong></p><p>TIB Conti-Campus<br> Königsworther Platz 1 B<br> 30167 Hannover</p><p>Any data subject may contact our data protection officer directly regarding any and all questions and suggestions regarding data protection at any time.</p><h3>Contact possibility via the web pages</h3><p>The TIB’s web pages contain information and forms that enable quick electronic contacting via contact form or by email providing name and email address also for customers not registered with the TIB. The indication of name and email address is necessary to establish contact following clarification of the data subject’s concerns. The personal data transmitted by the data subject to the TIB via email or via a contact form are automatically stored internally and only for the purpose of processing or contacting the data subject.</p><h3>Cookies</h3><p>This website does not use cookies.</p><h3>Use of web analysis tools</h3><p>For the creation of the web statistics we use the open source software Matomo (formerly PIWIK) in anonymized form. This means that no personal data is processed. The use of cookies in the web analysis software Matomo is deactivated.</p><p>When saving the user IP address, the last two octets are not processed. The collection of the user ID is deactivated. For the analysis, the following data is collected in addition to the access to the site and the anonymized IP address: Date and time of the request, page title of the requested page, URL of the previously requested page (referrer URL), screen resolution of the client system, local time zone, URL of clicked and downloaded files, URL of clicked external domains, geolocation of the client (country, region, city), main language of the used browser, user agent of the used browser.</p></div></div></div><div id="c241" class="frame frame-default frame-type-html frame-layout-0"><p><b>Matomo</b></p><p>The information generated with Matomo about the use of this website is processed and stored exclusively with the TIB.</p><iframe style="border: 0; height: 150px; width: 100%;" src="https://support.tib.eu/piwik/index.php?module=CoreAdminHome&amp;action=optOut&amp;language=en&amp;backgroundColor=ffffff&amp;fontColor=&amp;fontSize=14px&amp;fontFamily=%22Helvetica%20Neue%22%2CHelvetica%2CArial%2Csans-serif" qedbctylh=""></iframe><p>In this case, an opt-out cookie is placed in the browser of the data subject, which prevents Matomo from storing usage data. When the cookies are deleted, the Matomo opt-out cookie is also deleted. Such objection (opt-out) must be redeclared when visiting the TIB website again</p></div><div id="c242" class="frame frame-default frame-type-textmedia frame-layout-0"><div class="ce-textpic ce-right ce-intext"><div class="ce-bodytext"><h3>Collection of general data and information (logfiles)</h3><p>Whenever a data subject calls the TIB web pages, these pages automatically collect information in so-called server log files, which your browser automatically transmits to the TIB. This is:</p><ul><li>Browser type and browser version</li><li>Operating system used</li><li>Referrer URL</li><li>Hostname of the accessing computer</li><li>IP address</li><li>Internet service provider of the accessing system</li><li>Time of the server request</li></ul><p>As a general rule, this data is not attributable to a particular person. This data will not be merged with other data sources.</p><h3>Routine deletion and blocking of personal data</h3><p>The TIB processes and stores the data subject’s personal data only for the period necessary to achieve the purpose of such storage and in accordance with the General Data Protection Regulation and the country and institution-specific data protection regulations applicable to the TIB. Thereafter, the personal data will routinely be blocked or deleted in accordance with the statutory provisions.</p><h3>Rights of the data subject</h3><p>You shall at any time be entitled to obtain information about the data stored in this library, its origin and recipient and about the purpose of such data processing, as well as to rectification or erasure or restriction of processing or – to the extent that such processing is based on your consent – a right of withdrawal, possibly a right of objection and the right to data portability. Complaints may be lodged with the above-mentioned supervisory authority. You can contact us at any time for further questions on the subject of personal data.</p></div></div></div></div>
   </v-container>
 </template>
 
