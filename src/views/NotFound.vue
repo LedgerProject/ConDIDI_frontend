ab8086882d16f4d0a9dae2542e39fc61beccbfcc
Tim Moehring <tim.moehring@prime-force.com>
2021-06-09 20:47:36 +0200

    wip 404

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,14 +1,16 @@
 <template lang="pug">
   v-container(class="fill-height")
-    v-row(class="fill-height" align="center" justify="center")
-      v-col(cols="12" class="text-center")
-        h1(class="text-h2") Oh now, the resource could not be found.
-      v-col(cols="12" class="text-center")
-        v-img(alt="Searching for route path" eager max-height="50vh" src="img/clip-2.png")
-      v-col(cols="12" class="text-center")
-        v-btn(@click="$router.back()" text)
-          v-icon mdi-arrow-left
-          | Back to previous page
+    v-row(align="center" justify="center")
+      v-col(cols="12" md="6" class="text-start")
+        v-row
+          v-col(cols="12" class="text-start")
+            h1(class="text-h1") Oh no! Error 404
+          v-col(cols="12" class="text-start")
+            p(class="text-h5") The page you are looking for could not be found ...
+          v-col(cols="12")
+            v-btn(@click="$router.back()" text)
+              v-icon mdi-arrow-left
+              | Back to previous page
 </template>
 
 <script>
